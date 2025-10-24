2d9a314ae75174b97c6978fad099e9d0436bc672
Muhammad Umar <umarbinzulfiqar29@gmail.com>
2025-10-24 15:55:02 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,48 @@
 /*=============== HOME SPLIT TEXT ===============*/
+const { animate, text, stagger } = anime;
 
+const { chars: chars1 } = text.split('.home__profession-1', { chars: true })
+const { chars: chars2 } = text.split('.home__profession-2', { chars: true })
+
+animate(chars1, {
+  y: [
+    { to: ['100%', '0%'] },
+    { to: '-100%', delay: 4000, ease: 'in(3)' }
+  ],
+  duration: 900,
+  ease: 'out(3)',
+  delay: stagger(80),
+  loop: true,
+});
+animate(chars2, {
+  y: [
+    { to: ['100%', '0%'] },
+    { to: '-100%', delay: 4000, ease: 'in(3)' }
+  ],
+  duration: 900,
+  ease: 'out(3)',
+  delay: stagger(80),
+  loop: true,
+});
 
 /*=============== SWIPER PROJECTS ===============*/
+const swiperProjects = new Swiper('.projects__swiper', {
+ 
+  loop: true,
+  spaceBetween: 24,
+  slidesPerview: 'auto',
+  grabCursor: true,
+  speed:600,
 
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  }
+})
 
 /*=============== WORK TABS ===============*/
 
